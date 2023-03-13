c01fe80fa7d6d1d2a0badd6acc9d6ef94c379146
Churnobyl <tjdcjfals@gmail.com>
2023-03-14 08:53:20 +0900

    First commit
    Make layout for the attached mission

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify  # 웹개발 라이브러리
+from pymongo import MongoClient  # DB 라이브러리
+app = Flask(__name__)
+
+client = MongoClient(
+    'mongodb+srv://sparta:test@cluster0.caexhck.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta  # DB명
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
